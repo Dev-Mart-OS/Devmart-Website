18b69c494e447d29e949db5b8045c38d08e25bdd
Raakka <jrip03@gmail.com>
2021-05-08 03:14:30 -0400

    typo (also last commit was blog fixes)

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home(props) {
         <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Test Post</span>{' '}
+                <span className="block xl:inline">Welcome to</span>{' '}
                 <span className="block text-transparent bg-clip-text bg-gradient-to-br from-purple-400 via-pink-500 to-red-500 xl:inline">Devmart</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
